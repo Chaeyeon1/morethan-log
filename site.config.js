328ed636539ab212895e688506e9e3e975780d69
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2025-05-19 21:48:08 +0900

    fix : since 추가

--- site.config.js
+++ site.config.js
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  // since: 2025, // If leave this empty, current year will be used.
+  since: 2025, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "/유치원 몰티즈.jpeg", // The link to generate OG image, don't end with a slash
 
