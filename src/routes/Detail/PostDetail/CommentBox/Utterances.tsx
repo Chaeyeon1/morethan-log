b9ebe5e20413395e8d28c7b2bccd1c7f5fadce4d
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2025-05-26 21:45:21 +0900

    fix : sitemap 지우기

--- src/routes/Detail/PostDetail/CommentBox/Utterances.tsx
+++ src/routes/Detail/PostDetail/CommentBox/Utterances.tsx
@@ -15,7 +15,7 @@ const Utterances: React.FC<Props> = ({ issueTerm }) => {
   const router = useRouter()
 
   useEffect(() => {
-    const theme = scheme === "dark" ? "photon-dark" : "github-light"
+    const theme = `github-${scheme}`
     const script = document.createElement("script")
     const anchor = document.getElementById("comments")
     if (!anchor) return
